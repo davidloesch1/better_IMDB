09b1713e56aff0f9e789ad9b329607ececd5e5ff
Jen Valladares <lilspikey333@yahoo.com>
2020-02-11 11:43:30 -0600

    models, migrations, and admin screen done

--- better_IMDB/admin.py
+++ better_IMDB/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie, Role, Genre, Movie_Person, Movie_Person_Role
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Role)
+admin.site.register(Genre)
+admin.site.register(Movie_Person)
+admin.site.register(Movie_Person_Role)
