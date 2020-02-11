31753899b9cb09e67ec0b24b4441c7685d84a91b
Jen Valladares <lilspikey333@yahoo.com>
2020-02-11 16:58:39 -0600

    backend complete

--- seed-data/actors.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO directors
-    (name, date_of_birth, country_of_birth)
-VALUES
-    ('Steven Spielberg', '1946-12-18', ' United States');
--- seed-data/movies.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO directors
-    (country_of_birth)
-VALUES
-    ('Steven Spielberg', '1946-12-18', ' United States');
