86d0dbc7debac93617677a7ebda8d47085453b00
Joe Allen <allenjosephs@johndeere.com>
2020-02-11 16:54:45 -0600

    actors

--- seed-data/actors.sql
+++ seed-data/actors.sql
@@ -1,4 +1,16 @@
-INSERT INTO directors
+INSERT INTO actors
     (name, date_of_birth, country_of_birth)
 VALUES
-    ('Steven Spielberg', '1946-12-18', ' United States');
+    ('Leonardo DiCaprio', '1974-11-11', 'United States');
+INSERT INTO actors
+    (name, date_of_birth, country_of_birth)
+VALUES
+    ('Brad Pitt', '1963-12-18', 'United States');
+INSERT INTO actors
+    (name, date_of_birth, country_of_birth)
+VALUES
+    ('Robert De Niro', '1943-08-17', 'United States');
+INSERT INTO actors
+    (name, date_of_birth, country_of_birth)
+VALUES
+    ('Tom Hanks', '1956-07-09', 'United States');
